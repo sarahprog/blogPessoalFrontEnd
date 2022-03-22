11609b66681952c38c0fa3d2760c258a74380533
unknown <sarahfigueiraramos@gmail.com>
2022-03-22 17:37:16 -0300

    atualizacao

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://contosdehorroreterror.files.wordpress.com/2017/02/capoeira.jpg" 
+            alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
