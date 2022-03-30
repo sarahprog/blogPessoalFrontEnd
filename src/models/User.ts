ef7fad7041ba430a66b50ee39ca5b7aaadbb8f79
unknown <sarahfigueiraramos@gmail.com>
2022-03-29 21:55:31 -0300

    Implementando a segurança com login e cadastro

---
+++ src/models/User.ts
@@ -0,0 +1,11 @@
+interface User {
+    id: number;
+    nome: string;
+    usuario: string;
+    senha: string;
+    foto: string;
+}
+
+export default User;
+
+//model de cadastro de usuário
---
+++ src/services/Service.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'https://guiajuninhofullstack.herokuapp.com/'
+})
+
+export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data)
+}
+
+export const login = async(url: any, dados: any, setDado: any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data.token)
+}
+
+//cadastro de usuário
